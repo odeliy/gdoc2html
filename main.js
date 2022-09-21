b70a29bbb885d1b28bcbf50b09ab872774f5cae9
cody iott <codyleeiott@gmail.com>
2022-09-21 11:38:41 -0700

    build: working bare minimum prototype

--- main.js
+++ main.js
@@ -1,35 +1,34 @@
-import {
-	removeMetaTags,
-	swapSpans,
-	removeSpans,
-	formatLinks,
-	formatListItems,
-	removeParagraphTags,
-	removeMultipleBreaks
-} from './formatFunctions.js'
+import { formatLinks, removeTag, stripTagAttributes, swapTags } from './formatters.js'
+import { detectBold, detectItalic } from './detectors.js'
 
 function formatHTML(input) {
-	let metaTagsRemoved = removeMetaTags(input)
-	let spansSwapped = swapSpans(metaTagsRemoved)
-	let spansRemoved = removeSpans(spansSwapped)
-	let linksFormatted = formatLinks(spansRemoved)
-	let listItemsFormatted = formatListItems(linksFormatted)
-	let pTagsRemoved = removeParagraphTags(listItemsFormatted)
-	let doubleBreaksRemoved = removeMultipleBreaks(pTagsRemoved)
-	return doubleBreaksRemoved
+	let newString = ''
+
+	newString = removeTag(input, 'meta')
+	newString = removeTag(newString, 'b')
+	newString = removeTag(newString, 'div')
+	newString = removeTag(newString, 'br')
+	newString = removeTag(newString, 'colgroup')
+	newString = removeTag(newString, 'col')
+	newString = swapTags(newString, 'span', 'strong', detectBold)
+	newString = swapTags(newString, 'span', 'em', detectItalic)
+	newString = removeTag(newString, 'span')
+	newString = stripTagAttributes(newString)
+	newString = formatLinks(newString)
+
+	return newString
 }
 
 function pasteClipboard(event) {
 	let originalHTML = event.clipboardData.getData('text/html')
 	let formattedHTML = formatHTML(originalHTML)
-	document.getElementById('app').innerHTML = formattedHTML
-	console.log(formattedHTML)
+	document.getElementById('app').innerText = formattedHTML
 }
 
 window.addEventListener('paste', (e) => pasteClipboard(e))
 
 window.addEventListener('keypress', (e) => {
-	if(e.key === 'Enter') {
+	if (e.key === 'Enter') {
 		document.getElementById('app').innerHTML = `
 			<p>ready to roll..</p>
 		`
