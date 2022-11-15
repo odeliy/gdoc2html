33d7d025ae055f145e5896deb17c0f8cd6ff8a48
cody iott <codyleeiott@gmail.com>
2022-11-15 10:38:09 -0800

    fix: disable toc toggle in CC mode

--- js/setup.js
+++ js/setup.js
@@ -1,72 +1,116 @@
-function setupToggle(
-  nodeIndex,
-  toggleOuter,
-  toggleInner,
-  setting,
-  toggle1,
-  toggle2,
-  value1,
-  value2
-) {
-  let platformSelected = localStorage.getItem(setting) || value1
-
-  const option1 = document.getElementById(toggle1)
-  const option2 = document.getElementById(toggle2)
-
-  if (platformSelected === value1) {
-    option1.classList.add('selected')
-    option2.classList.remove('selected')
-    toggleInner[nodeIndex].classList.remove('flip-toggle')
-  } else if (platformSelected === value2) {
-    option1.classList.remove('selected')
-    option2.classList.add('selected')
-    toggleInner[nodeIndex].classList.add('flip-toggle')
-  }
-
-  toggleOuter[nodeIndex].addEventListener('click', () => {
-    platformSelected = localStorage.getItem(setting) || value1
-
-    if (platformSelected === value1) {
-      option1.classList.remove('selected')
-      option2.classList.add('selected')
-      toggleInner[nodeIndex].classList.add('flip-toggle')
-      localStorage.setItem(setting, value2)
-    } else if (platformSelected === value2) {
-      option1.classList.add('selected')
-      option2.classList.remove('selected')
-      toggleInner[nodeIndex].classList.remove('flip-toggle')
-      localStorage.setItem(setting, value1)
-    }
-  })
-}
-
 export default function setup() {
-  let toggleOuter = document.querySelectorAll('.toggle-outer')
-  let toggleInner = document.querySelectorAll('.toggle-inner')
+  const websiteToggleOuter = document.getElementById('websiteToggleOuter')
+  const websiteToggleInner = document.getElementById('websiteToggleInner')
+  const toggleBR = document.getElementById('toggleBR')
+  const toggleCC = document.getElementById('toggleCC')
 
-  const settings = [
-    ['website', 'toggleBR', 'toggleCC', 'bankrate.com', 'creditcards.com'],
-    ['tables', 'toggleTableOn', 'toggleTableOff', 'on', 'off'],
-    ['toc', 'toggleTocOn', 'toggleTocOff', 'on', 'off']
+  const tablesToggleOuter = document.getElementById('tablesToggleOuter')
+  const tablesToggleInner = document.getElementById('tablesToggleInner')
+  const toggleTableOn = document.getElementById('toggleTableOn')
+  const toggleTableOff = document.getElementById('toggleTableOff')
+
+  const tocToggleOuter = document.getElementById('tocToggleOuter')
+  const tocToggleInner = document.getElementById('tocToggleInner')
+  const toggleTocOn = document.getElementById('toggleTocOn')
+  const toggleTocOff = document.getElementById('toggleTocOff')
+
+  const defaults = [
+    ['website', 'bankrate.com'],
+    ['tables', 'on'],
+    ['toc', 'off']
   ]
 
-  settings.forEach((setting) => {
-    const doesThisExistYet = localStorage.getItem(setting[0])
-    if (!doesThisExistYet) {
-      localStorage.setItem(setting[0], setting[3])
+  defaults.forEach((element) => {
+    const alreadyInLocalStorage = localStorage.getItem(element[0])
+    if (!alreadyInLocalStorage) {
+      localStorage.setItem(element[0], element[1])
     }
   })
 
-  settings.forEach((setting, index) => {
-    setupToggle(
-      index,
-      toggleOuter,
-      toggleInner,
-      setting[0],
-      setting[1],
-      setting[2],
-      setting[3],
-      setting[4]
-    )
+  // website toggle
+  if (localStorage.getItem('website') === 'bankrate.com') {
+    toggleBR.classList.add('selected')
+    toggleCC.classList.remove('selected')
+    websiteToggleInner.classList.remove('flip-toggle')
+    tocToggleInner.classList.remove('disabled')
+  } else if (localStorage.getItem('website' === 'creditcards.com')) {
+    toggleBR.classList.remove('selected')
+    toggleCC.classList.add('selected')
+    websiteToggleInner.classList.add('flip-toggle')
+    tocToggleInner.classList.add('disabled')
+    tocToggleOuter.removeEventListener('click', tocEventListener)
+  }
+
+  websiteToggleOuter.addEventListener('click', () => {
+    let websiteSelection = localStorage.getItem('website')
+    if (websiteSelection === 'bankrate.com') {
+      toggleBR.classList.remove('selected')
+      toggleCC.classList.add('selected')
+      websiteToggleInner.classList.add('flip-toggle')
+      tocToggleInner.classList.add('disabled')
+      tocToggleOuter.removeEventListener('click', tocEventListener)
+      localStorage.setItem('website', 'creditcards.com')
+    } else if (websiteSelection === 'creditcards.com') {
+      toggleBR.classList.add('selected')
+      toggleCC.classList.remove('selected')
+      websiteToggleInner.classList.remove('flip-toggle')
+      tocToggleInner.classList.remove('disabled')
+      tocToggleOuter.addEventListener('click', tocEventListener)
+      localStorage.setItem('website', 'bankrate.com')
+    }
   })
+
+  // tables toggle
+  if (localStorage.getItem('tables') === 'on') {
+    toggleTableOn.classList.add('selected')
+    toggleTableOff.classList.remove('selected')
+    tablesToggleInner.classList.remove('flip-toggle')
+  } else if (localStorage.getItem('tables') === 'off') {
+    toggleTableOn.classList.remove('selected')
+    toggleTableOff.classList.add('selected')
+    tablesToggleInner.classList.add('flip-toggle')
+  }
+
+  tablesToggleOuter.addEventListener('click', () => {
+    let tablesSelection = localStorage.getItem('tables')
+    if (tablesSelection === 'on') {
+      toggleTableOn.classList.remove('selected')
+      toggleTableOff.classList.add('selected')
+      tablesToggleInner.classList.add('flip-toggle')
+      localStorage.setItem('tables', 'off')
+    } else if (tablesSelection === 'off') {
+      toggleTableOn.classList.add('selected')
+      toggleTableOff.classList.remove('selected')
+      tablesToggleInner.classList.remove('flip-toggle')
+      localStorage.setItem('tables', 'on')
+    }
+  })
+
+  // toc toggle
+  if (localStorage.getItem('toc') === 'on') {
+    toggleTocOn.classList.add('selected')
+    toggleTocOff.classList.remove('selected')
+    tocToggleInner.classList.remove('flip-toggle')
+  } else if (localStorage.getItem('toc') === 'off') {
+    toggleTocOn.classList.remove('selected')
+    toggleTocOff.classList.add('selected')
+    tocToggleInner.classList.add('flip-toggle')
+  }
+
+  function tocEventListener() {
+    let tablesSelection = localStorage.getItem('toc')
+    if (tablesSelection === 'on') {
+      toggleTocOn.classList.remove('selected')
+      toggleTocOff.classList.add('selected')
+      tocToggleInner.classList.add('flip-toggle')
+      localStorage.setItem('toc', 'off')
+    } else if (tablesSelection === 'off') {
+      toggleTocOn.classList.add('selected')
+      toggleTocOff.classList.remove('selected')
+      tocToggleInner.classList.remove('flip-toggle')
+      localStorage.setItem('toc', 'on')
+    }
+  }
+
+  tocToggleOuter.addEventListener('click', tocEventListener)
 }
